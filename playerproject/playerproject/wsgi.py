fd37959c9f79b139e133f2975af45e6349bbc7f3
KirushanthS <kirushanth.2@gmail.com>
2013-11-03 21:02:53 -0500

    remove uwsgi test

--- playerproject/playerproject/wsgi.py
+++ playerproject/playerproject/wsgi.py
@@ -7,24 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
-# import os
-# import sys
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "playerproject.settings")
-
-# APP_DIR = os.path.abspath(os.path.dirname(__file__)).replace('\\','/')
-# sys.path.append(APP_DIR)
-# print APP_DIR
-# output = 'sys.path = %s' % repr(sys.path)
-# print [output]
-
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# import uwsgi
-# from uwsgidecorators import timer
-# from django.utils import autoreload
+import uwsgi
+from uwsgidecorators import timer
+from django.utils import autoreload
 
-# @timer(1)
-# def change_code_gracefull_reload(sig):
-#     if autoreload.code_changed():
-#         uwsgi.reload()
+@timer(1)
+def change_code_gracefull_reload(sig):
+    if autoreload.code_changed():
+        uwsgi.reload()
