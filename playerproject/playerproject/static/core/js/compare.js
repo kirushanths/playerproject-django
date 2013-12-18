b4b8eaf56613e74ed47b07fd6e3e0fba2da3ea5b
Ajun <ajunpreet@gmail.com>
2013-12-17 23:33:26 -0500

    Add ability to remove players, TODO: Add New players

---
+++ playerproject/playerproject/static/core/js/compare.js
@@ -0,0 +1,75 @@
+function get_random_color() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+
+//Function to create a legend
+function legend(parent, data) {
+    parent.className = 'legend';
+    var datas = data.hasOwnProperty('datasets') ? data.datasets : data;
+
+    datas.forEach(function(d) {
+        var title = document.createElement('span');
+        title.className = 'title';
+        title.style.borderColor = d.hasOwnProperty('fillColor') ? d.fillColor : d.color;
+        title.style.borderStyle = 'solid';
+        parent.appendChild(title);
+        var text = document.createTextNode(d.title);
+        title.appendChild(text);
+    });
+
+}
+Array.prototype.remove = function(set){return this.filter(
+    function(e,i,a){return set.indexOf(e)<0}
+)};
+
+domready(function() {
+	var active_players = [];
+	$('.player-active').each(function(index, value){
+		active_players.push($(this).attr('data-record'));
+	});
+
+	var _managerCntrl = window.ManagerCntrl = {
+		$selectToolbar: $('#manager-record-controls'),
+		$compareButton: $('#manager-compare'),
+		selected: [],
+	};
+
+	_managerCntrl.$selectToolbar.hide();
+
+	$('.record-checkbox').click(function(e) {
+		var control = ManagerCntrl;
+		var toolbar = control.$selectToolbar;
+		var compare = control.$compareButton;
+		var array = control.selected;
+		var element = $(this);
+		var found = false;
+		var id = element.attr('data-record');
+		for (var i = array.length - 1; i >= 0; i--) {
+
+			if (array[i] == id) {
+				array.splice(i, 1);
+				active_players.push(id);
+				element.find('i').addClass('glyphicon-unchecked').css('color','lightgreen');
+				element.find('i').removeClass('glyphicon-ok');
+				found = true;
+			}
+		};
+
+		if (!found) {
+			array.push(id);
+			element.find('i').addClass('glyphicon-remove').css('color','red');
+			element.find('i').removeClass('glyphicon-unchecked');
+		}
+
+		toolbar.toggle(array.length > 0);
+		active_players = active_players.remove(array);
+		console.log(active_players);
+		compare.attr('href', compare.attr('data-django').replace('abc', active_players.join('/')));
+	});
+
+});
